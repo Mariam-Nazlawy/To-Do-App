65b37f9d4ec2f2a4bc501e32131c9fb37b7627c6
DELL <maryamnzlawy@gmail>
2024-07-25 10:57:43 +0300

    add tasks' routes definitions

--- backend/src/routes/tasks.js
+++ backend/src/routes/tasks.js
@@ -1,3 +1,29 @@
 const express = require('express');
-const tasksRoute = express.Router();
+const tasksRoutes = express.Router();
 
+// create new task
+tasksRoutes.post('/', (req, res) => {
+    
+})
+
+//  Retrieve all tasks for the authenticated user
+tasksRoutes.get('/', (req, res) => {
+    res.send("hello from tasks endpoint!");
+})
+
+// Retrieve a specific task by ID
+tasksRoutes.get('/:id', (req, res) => {
+    
+})
+
+//  Update a task by ID
+tasksRoutes.put('/:id', (req, res) => {
+    
+})
+
+// Mark a task as completed
+tasksRoutes.patch('/:id/complete', (req, res) => {
+
+})
+
+module.exports = tasksRoutes;
